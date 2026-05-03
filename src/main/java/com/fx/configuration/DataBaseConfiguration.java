95997463f1f28a255dde3ef412eb73996f542fcf
KolaBaka <kolamba040301@gmail.com>
2026-05-03 17:19:22 +0300

    chapter 6 configuration, configuration only

--- src/main/java/com/fx/configuration/DataBaseConfiguration.java
+++ src/main/java/com/fx/configuration/DataBaseConfiguration.java
@@ -6,11 +6,13 @@ import com.fx.repository.jpa.IngredienRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DataBaseConfiguration {
 
     @Bean //like post construct but for application context, work after all beans configurations
+    @Profile("dev") // or could use @Profile("!dev")
     public ApplicationRunner dataLoader(IngredienRepository ingredienRepository) {
         return args -> {
             ingredienRepository.save(new Ingredient("CHC", "chicken", Ingredients.CHICKEN));
--- src/main/java/com/fx/controller/CommentController.java
+++ src/main/java/com/fx/controller/CommentController.java
@@ -3,8 +3,9 @@ package com.fx.controller;
 import com.fx.dto.mongo.Comment;
 import com.fx.repository.mongo.CommentRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class CommentController {
 
     @GetMapping
     public List<Comment> getAllComments(){
+//        Pageable page = PageRequest.of(0, 20); //for paging could use Spring interface Pageable
         return commentRepository.findAll();
     }
 }
